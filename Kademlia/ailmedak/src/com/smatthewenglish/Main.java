b41d35cc135b10d7ac4ef295f4a65b29a49ffbe4
s.matthew.english <s.matthew.english@gmail.com>
2018-04-14 19:30:51 -0400

    14.04.2018

--- Kademlia/ailmedak/src/com/smatthewenglish/Main.java
+++ Kademlia/ailmedak/src/com/smatthewenglish/Main.java
@@ -1,10 +1,52 @@
 package com.smatthewenglish;
 
+import java.util.ArrayList;
+
 public class Main {
 
+    private static Object[] kBucket  = new Object[8];
 
     public static void main(String[] args) {
-      // write your code here
-        System.out.println("Hello world!");
+
+        Object obj0 = 'M';
+        Object obj1 = 'A';
+        Object obj2 = 'T';
+        Object obj3 = 'T';
+        Object obj4 = 'H';
+        Object obj5 = 'E';
+        Object obj6 = 'W';
+
+        kBucket[0] = obj0;
+        kBucket[1] = obj1;
+        kBucket[2] = obj2;
+        kBucket[3] = obj3;
+        kBucket[4] = obj4;
+        kBucket[5] = obj5;
+        kBucket[6] = obj6;
+
+        for (int i = 0; i < kBucket.length; i++)
+            System.out.println("Element at index " + i + " : "+ kBucket[i]);
+
+        System.out.println("-----------------");
+
+        ausschlagen(obj0);
+
+        for (int i = 0; i < kBucket.length; i++)
+            System.out.println("Element at index " + i + " : "+ kBucket[i]);
+
+    }
+
+    public static void ausschlagen(Object bandit) {
+        for (int index = 0; index < kBucket.length; index++) {
+            if (kBucket[index].equals(bandit))
+                System.out.println(kBucket[index]);
+                System.out.println(bandit);
+                System.arraycopy(kBucket, index + 1, kBucket, index, (kBucket.length - index - 1));
+        }
+
+
     }
 }
+
+
+
