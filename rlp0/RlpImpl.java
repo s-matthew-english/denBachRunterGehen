2af3dd54823065f7284393beed3747727196daae
s.matthew.english <s.matthew.english@gmail.com>
2018-05-01 20:26:25 -0400

    01.04.2018

--- rlp0/RlpImpl.java
+++ rlp0/RlpImpl.java
@@ -7,30 +7,60 @@ public class RlpImpl {
   public static void main(String... args) {
     System.out.println("Hi Zac!");
 
-    ArrayList<Object> input0 = new ArrayList<>();
+    // [ 00, 0102, [[03, 04],[05]] ]
+    //
+    // [ 0102, [[03, 04],[05]] ]
+
+    ArrayList<Object> input = new ArrayList<>();
     ArrayList<Object> input1 = new ArrayList<>();
     ArrayList<Object> input2 = new ArrayList<>();
     ArrayList<Object> input3 = new ArrayList<>();
 
-    input0.add("01");
-    input0.add("02");
-    input1.add("03");
-    input1.add("04");
-    input2.add("05");
+
+    byte[] input0 = new byte[] {0x01,0x02};
+    input1.add(new byte[] {0x03});
+    input1.add(new byte[] {0x04});
+    input2.add(new byte[] {0x05});
+
+    // input0.add("02");
+    // input1.add("03");
+    // input1.add("04");
+    // input2.add("05");
 
     input3.add(input1);
     input3.add(input2);
 
-    input0.add(input3);
+    input.add(input0);
+    input.add(input3);
 
     // System.out.println(input0.toString());
 
 
-    System.out.println(encode(input0));
+    System.out.println(pr(input));
 
     // System.out.println(Arrays.toString("1".getBytes()));
 
     // System.out.println(toBinary(65));
+  }
+
+
+  public static String pr(Object xinput) {
+
+    String output = "";
+
+    if (xinput instanceof ArrayList<?>) {
+      output += "[";
+      ArrayList<Object> input = cast(xinput);
+      for (Object element : input) {
+        output +=  pr(element) + ",";
+
+      }
+      output = output.endsWith(",") ? output.substring(0, output.length() - 1) : output;
+      output += "]";
+    }else{
+      output = DatatypeConverter.printHexBinary((byte[])xinput);
+    }
+    return output;
   }
 
 
