4340ebed8c2f338f1121721e798b57ef3f842a6c
s.matthew.english <s.matthew.english@gmail.com>
2018-05-06 12:33:08 -0400

    06.05.2018

--- Netty/src/main/java/hello/HelloNetty.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-public class HelloNetty {
-  public static void main(String[] args) {
-    System.out.println("Let's get it");
-  }
-}
--- NettyInAction/ConnectHandler.java
+++
@@ -1,6 +0,0 @@
-public class ConnectHandler extends ChannelInboundAdapter {
-  @Override
-  public void channelActive(ChannelHandlerContext ctx) throws Exception {
-    System.out.println("Client " + ctx.channel().remoteAddress() + " connected");
-  }
-}
